c0a309d7f59d3635b619df17086e1a3661be8414
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-12-02 12:18:26 +0400

    create new component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import './App.css';
 const App = () => {
   return (
     <div>
-        <div>one div</div>
-        <div className="App">
-          <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>react</li>
-          </ul>
-        </div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href='#'>Home</a>
+      <a href='#'>News feef</a>
+      <a href='#'>Messagers</a>
+    </div>
+  );
+}
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
   );
 }
