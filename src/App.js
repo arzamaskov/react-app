97c3cfc701fc5985848d7fa354ce994128b57a6b
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-12-05 17:58:31 +0400

    split application with components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='./logo.svg' alt='logo' />
-      </header>
-      <nav className='nav'>
-        <div><a href="#">Profile</a></div>
-        <div><a href='#'>Messages</a></div>
-        <div><a href='#'>News</a></div>
-        <div><a href='#'>Music</a></div>
-        <div><a href='#'>Settings</a></div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src='#' alt='main content background' />
-        </div>
-        <div>
-          ava + descr
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-          <div>
-            <div>
-              post 1
-            </div>
-            <div>
-              post 2
-            </div>
-          </div>
-        </div>
-        Main content
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
