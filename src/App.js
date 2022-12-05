58b25642cf09ceaa57b3c39bb46c3a61a983255f
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-12-05 17:03:20 +0400

    delete unusual components and create html-structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies.js';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='./logo.svg' alt='logo' />
+      </header>
+      <nav className='nav'>
+        <div><a href="#">Profile</a></div>
+        <div><a href='#'>Messages</a></div>
+        <div><a href='#'>News</a></div>
+        <div><a href='#'>Music</a></div>
+        <div><a href='#'>Settings</a></div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='#' alt='main content background' />
+        </div>
+        <div>
+          ava + descr
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+        Main content
+      </div>
     </div>
   );
 }
